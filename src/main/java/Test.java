2972ab2969c482dad06a650cf40f4e1cd5a08f72
David Korpos <korposdavid@gmail.com>
2019-10-28 10:40:39 +0100

    separate handlers for index and test routes

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -10,16 +10,27 @@ public class Test {
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new MyHandler());
-        server.createContext("/index", new MyHandler());
+        server.createContext("/test", new testHandler());
+        server.createContext("/index", new indexHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
-    static class MyHandler implements HttpHandler {
+    static class testHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
+            String response = "This is the test page";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    static class indexHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the index page";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
