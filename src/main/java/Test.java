e956be850ba29a5d1d1a89545d6e06ed783854d6
David Korpos <korposdavid@gmail.com>
2019-10-28 11:59:20 +0100

    add methodToInvoke field to myHandler

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,14 +8,13 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class Test {
-
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
         for (Method m : Routes.class.getMethods()) {
             if (m.isAnnotationPresent(WebRoute.class)) {
                 String path = m.getAnnotation(WebRoute.class).path();
-                server.createContext(path, new myHandler());
+                server.createContext(path, new myHandler(m));
             }
         }
 
@@ -24,23 +23,22 @@ public class Test {
     }
 
     static class myHandler implements HttpHandler {
+        Method methodToInvoke;
+
+        public myHandler(Method methodToInvoke) {
+            this.methodToInvoke = methodToInvoke;
+        }
+
         @Override
         public void handle(HttpExchange t) {
-            for (Method m : Routes.class.getMethods()) {
-                if (m.isAnnotationPresent(WebRoute.class)) {
-                    if (m.getAnnotation(WebRoute.class).path().equals(t.getHttpContext().getPath())) {
-                        try {
-                            String response = (String) m.invoke(Routes.class.newInstance());
-                            t.sendResponseHeaders(200, response.length());
-                            OutputStream os = t.getResponseBody();
-                            os.write(response.getBytes());
-                            os.close();
-                            return;
-                        } catch (Throwable e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
+            try {
+                String response = (String) methodToInvoke.invoke(Routes.class.newInstance());
+                t.sendResponseHeaders(200, response.length());
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            } catch (Throwable e) {
+                e.printStackTrace();
             }
         }
     }
